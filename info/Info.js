3e0f08378aae9d1689a0aadc20d50e5fdda31128
Gabriel Turrillas <gabrielturrillasdev@gmail.com>
2021-08-15 00:13:17 -0400

    Responsive

--- info/Info.js
+++ info/Info.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled, { createGlobalStyle } from 'styled-components';
+import { device } from './sizes';
 
 const Info = ({ imgPath, alt }) => {
   return (
@@ -26,23 +27,36 @@ const Info = ({ imgPath, alt }) => {
     </>
   )
 }
-const WorkArea = styled.div`
-  display: flex;
-  text-align:center;
-  justify-content:center;
-  flex-direction:column;
-  padding:0 1rem;
-`
 
 export default Info
 
 const Container = styled.div`
   display:grid;
   grid-template-columns:1fr;
-  grid-template-rows:1fr 1fr 2fr;
+  grid-template-rows:1fr 2fr 3fr;
+  grid-template-areas:'TitleArea'
+                      'TextArea'
+                      'ImgArea';
   justify-content:center;
   background:#010606;
   color:white;
+  @media ${device.mobileS}{
+    padding:0 2rem;
+  }
+  @media ${device.mobileL}{
+    grid-template-rows:1fr 2fr 3fr;
+  }
+  @media ${device.tablet}{
+    padding:0 5rem;
+  }
+  @media ${device.laptop}{
+    grid-template-areas:'TitleArea TitleArea'
+                        'TextArea ImgArea';
+    grid-template-rows: 1fr 3fr;
+  }
+  @media ${device.laptopL}{
+    padding:0 21rem;
+  }
 `
 
 const TitleArea = styled.div`
@@ -51,20 +65,20 @@ const TitleArea = styled.div`
   justify-content:center;
   align-items:center;
   text-align:center;
-  background:red;
-  `;
+  grid-area:TitleArea;
+  @media ${device.mobileL}{
+    font-size:5rem;
+    align-items:end;
+  }
+  @media ${device.mobileL}{
+    align-items:end;
+  }
+`;
 const Title = styled.h3`
-`;
-
-
-const ImgArea = styled.div`
-  display:flex;
-  justify-content:center;
-  padding:2rem;
-`;
-const Img = styled.img`
-  max-width: 100%;
-  height: auto;
+  font-size:3rem;
+  @media ${device.mobileL}{
+    font-size:5rem;
+  }
 `;
 
 
@@ -73,185 +87,35 @@ const TextArea = styled.div`
   text-align:center;
   justify-content:center;
   flex-direction:column;
+  font-size:18px;
+  padding:0 1rem;
+  grid-area:TextArea;
 `;
 const Text = styled.article`
   display:flex;
   height:100%;
-  background-color:red;
-  word-wrap: break-word
-`;
-
-
-const GlobalStyles = createGlobalStyle`
-* {
-  margin: 0;
-  padding: 0;
-  outline: 0;
-  box-sizing: border-box;
-  font-family: 'Roboto', sans-serif;
-}
-
-#root {
-  margin:0 auto;
-}
-`
-
-
-
-
-
-/* import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import { device } from './sizes';
-
-const Works = ({
-  english = true,
-  projectName = 'poietic',
-  description = 'Sunt id sint nulla consequat adipisicing ullamco esse qui sint ullamco consectetur. Duis esse non fugiat adipisicing occaecat. Culpa eiusmod eu veniam anim mollit eiusmod reprehenderit commodo irure aliquip mollit.Cillum commodo voluptate cillum consectetur laboris. Irure laboris eiusmod laboris ea exercitation adipisicing laborum ad. Laborum tempor excepteur laborum commodo consequat duis consequat eu eiusmod sunt reprehenderit occaecat enim ipsum. Enim cillum duis dolore culpa quis eu nisi non deserunt enim adipisicing.',
-  imgURL = './ClientSide.svg',
-  pageURL = '/some/url',
-  githubURL = '/some/url',
-  imgStart = false }) => {
-  return (
-    <>
-      <GlobalStyles />
-      <WorksContainer imgStart={imgStart}>
-        <WorkArea>
-          <ProjectName>{projectName}</ProjectName>
-          <Description>
-            {description}
-          </Description>
-        </WorkArea>
-        <ImgArea>
-          <Img src={imgURL} alt={projectName} />
-        </ImgArea>
-        <BtnsArea>
-          <Button
-            href={pageURL}
-            big={false}
-          > {english ? 'Go to the Page' : 'Ir a la Página'}
-          </Button>
-          <Button
-            href={githubURL}
-            big={false}
-          > {english ? 'Go to the Code' : 'Ir al Código'}
-          </Button>
-        </BtnsArea>
-      </WorksContainer>
-    </>
-  )
-}
-
-export default Works
-
-
-const BtnsArea = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:center;
-  padding:0 2rem;
+  word-wrap: break-word;
+  @media ${device.mobileL}{
+    align-items:center;
+    font-size:22px; 
+  }
   @media ${device.tablet}{
-    flex-direction:row;
   }
-  @media ${device.laptop} {
-    grid-area:buttons;
-  }
-`
+  `;
+
 
 const ImgArea = styled.div`
   display:flex;
   justify-content:center;
-  align-items:center;
-  @media ${device.laptop} {
-    grid-area:col2;
-  }
-
-  `
-
-const WorkArea = styled.div`
-  display: flex;
-  text-align:center;
-  justify-content:center;
-  flex-direction:column;
-  padding:0 1rem;
-  @media ${device.tablet}{
+  grid-area:ImgArea;
+  @media ${device.mobileM}{
     padding:2rem;
   }
-  @media ${device.laptop} {
-    grid-area:col1;
-  }
-`
-
+`;
 const Img = styled.img`
-  border-radius:5px;
-  width: 80%;
-  height:auto;
+  max-width: 100%;
+  height: auto;
 `;
-
-
-const ProjectName = styled.h3`
-  height: fit-content;
-  padding:1rem;
-  `
-
-const Description = styled.article`
-  display:flex;
-  height: fit-content;
-`
-
-const Button = styled.a`
-  border-radius: 50px;
-  background: ${({ primary }) => (primary ? '#01BF71' : '#010606')};
-  white-space: nowrap;
-  padding: ${({ big }) => (big ? '14px 48px' : '12px 30px')};
-  color: ${({ dark }) => (dark ? '#010606' : '#fff')};
-  font-size: ${({ fontBig }) => (fontBig ? '20px' : '16px')};
-  outline: none;
-  border: solid;
-  border-color:#01BF71;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: all 0.2 ease-in-out;
-  text-decoration: none;
-  margin:1rem;
-  &:hover {
-    transition: all 0.2 ease-in-out;
-    background: ${({ primary }) => (primary ? '#fff' : '#01BF71')};
-  };
-`
-
-const WorksContainer = styled.div`
-  color: #f7f8fa;
-  background: #010606;
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-rows: 2fr 1fr 1fr;
-  font-family:'Didact Gothic', sans-serif;
-  justify-content: center;
-  @media ${device.mobileS} {
-    grid-template-rows: repeat(3,1fr);
-  }
-  @media ${device.mobileM} {
-    justify-content:center;
-    grid-template-rows: repeat(3, 2fr);
-  }
-  @media ${device.mobileL} {
-    padding:0 2rem;
-  }
-  @media ${device.tablet}{
-    font-size:25px;
-    grid-template-rows: repeat(2, 2fr) 1fr;
-  }
-  @media ${device.laptop} {
-    grid-template-columns:repeat(2, 1fr);
-    grid-template-rows:2fr 1fr;
-    grid-template-areas: ${({ imgStart }) => (imgStart ? "'col2 col1' 'buttons buttons'" : "'col1 col2' 'buttons buttons'")}
-  }
-`;
-
 
 
 const GlobalStyles = createGlobalStyle`
@@ -262,8 +126,8 @@ const GlobalStyles = createGlobalStyle`
   box-sizing: border-box;
   font-family: 'Roboto', sans-serif;
 }
+
 #root {
   margin:0 auto;
 }
-` */
-
+`
--- info/sizes.js
+++ info/sizes.js
@@ -1,10 +1,10 @@
 const size = {
   mobileS: '320px',
-  mobileM: '375px',
-  mobileL: '425px',
-  tablet: '768px',
-  laptop: '1024px',
-  laptopL: '1440px',
+  mobileM: '375px', //375-64=311
+  mobileL: '425px', //425−64=361
+  tablet: '768px', //768−64=704
+  laptop: '1024px', //1024−160=864
+  laptopL: '1440px', //1440-160=1280
   desktop: '2560px'
 }
 
